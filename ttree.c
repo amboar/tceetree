06b80ad13a8266f02cd0bf2957d4f8f4550dd4d0
Andrew Jeffery <andrew@aj.id.au>
2017-06-26 00:17:41 +0930

    ttree: Return early in treeaddnode if node is present
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- ttree.c
+++ ttree.c
@@ -69,26 +69,27 @@ int ttreeaddnode(ttree_t *ptree, char *funname, char *filename)
 	int iErr = 0;
 	ttreenode_t *pnode;
 
-	if (ttreefindnode(ptree, funname, filename) == NULL) {
-		// only if node does not exist yet
-		pnode = (ttreenode_t *)malloc(sizeof(ttreenode_t));
-		if (!pnode) {
-			printf("\nMemory allocation error\n");
-			iErr = -1;
-		} else {
-			// initialize all node data bytes to 0
-			memset(pnode, 0, sizeof(ttreenode_t));
-			iErr = slibcpy(&pnode->funname, funname, -1);
+	if (ttreefindnode(ptree, funname, filename) != NULL)
+		return 0;
+
+	// only if node does not exist yet
+	pnode = (ttreenode_t *)malloc(sizeof(ttreenode_t));
+	if (!pnode) {
+		printf("\nMemory allocation error\n");
+		iErr = -1;
+	} else {
+		// initialize all node data bytes to 0
+		memset(pnode, 0, sizeof(ttreenode_t));
+		iErr = slibcpy(&pnode->funname, funname, -1);
+		if (iErr == 0) {
+			iErr = slibcpy(&pnode->filename, filename, -1);
 			if (iErr == 0) {
-				iErr = slibcpy(&pnode->filename, filename, -1);
-				if (iErr == 0) {
-					if (ptree->lastnode) {
-						ptree->lastnode->next = pnode;
-						ptree->lastnode = pnode;
-					} else {
-						ptree->firstnode = pnode;
-						ptree->lastnode = pnode;
-					}
+				if (ptree->lastnode) {
+					ptree->lastnode->next = pnode;
+					ptree->lastnode = pnode;
+				} else {
+					ptree->firstnode = pnode;
+					ptree->lastnode = pnode;
 				}
 			}
 		}
